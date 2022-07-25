0519e13210bc00d05e37b37444545d100ddd270a
Avish Jain <avishjain0@gmail.com>
2022-07-25 23:20:11 +0530

    register form

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import "./register.css";
 
 const Register = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert("form");
+  }
+  function handleChange(e) {}
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register">
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div className="brand">
+          <img src="/assets/logo.svg" alt="logo" />
+          <h1>snappy</h1>
+        </div>
+        <input
+          type="text"
+          placeholder="Username"
+          name="Username"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={(e) => handleChange(e)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="confirmPassword"
+          onChange={(e) => handleChange(e)}
+        />
+        <button type="submit">Create User</button>
+        <span>
+          Already have an account ? <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
