ab547d8c6300c0815863876be261910f0579cf76
Avish Jain <avishjain0@gmail.com>
2022-08-04 01:55:02 +0530

    backend

--- public/src/pages/Register.js
+++ public/src/pages/Register.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoutes";
 
 const Register = () => {
-  function handleSubmit(e) {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const toastOptions = {
+    position: "bottom-center",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+    theme: "dark",
+  };
+
+  async function handleSubmit(e) {
     e.preventDefault();
-    alert("form");
+    if (handleValidation()) {
+      const { username, email, password, confirmPassword } = values;
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+    }
   }
-  function handleChange(e) {}
+
+  function handleValidation(e) {
+    const { username, email, password, confirmPassword } = values;
+    if (password !== confirmPassword) {
+      toast.error("Passwords not macthing!", toastOptions);
+      return false;
+    } else if (username.length < 3) {
+      toast.error(
+        "Username should be greater than 3 characters!",
+        toastOptions
+      );
+      return false;
+    } else if (password.length < 8) {
+      toast.error(
+        "Password should be equal or greater than 8 characters!",
+        toastOptions
+      );
+      return false;
+    }
+    return true;
+  }
+
+  function handleChange(e) {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
   return (
     <div className="register">
       <form onSubmit={(e) => handleSubmit(e)}>
@@ -18,7 +68,7 @@ const Register = () => {
         <input
           type="text"
           placeholder="Username"
-          name="Username"
+          name="username"
           onChange={(e) => handleChange(e)}
         />
         <input
@@ -26,6 +76,7 @@ const Register = () => {
           placeholder="Email"
           name="email"
           onChange={(e) => handleChange(e)}
+          required
         />
         <input
           type="password"
@@ -44,6 +95,7 @@ const Register = () => {
           Already have an account ? <Link to="/login">Login</Link>
         </span>
       </form>
+      <ToastContainer />
     </div>
   );
 };
